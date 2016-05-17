93426497467f3029e65ec7182006a6bd557e76bd
Doctoryoung <1875200406@qq.com>
2016-05-17 20:31:17 +0800

    完成
    添加了kingMove算法

--- Amazon/src/amazon/AI.java
+++ Amazon/src/amazon/AI.java
@@ -10,11 +10,44 @@ public class AI {
 	public static final int OBSTACLE    = 3;    //障碍
 	public int aI;
 	private int whoIsAI ;
+	private int[]  chess       = new int[8];     //用来存放皇后的种类及位置信息
+	
 	private int[][] boardArray = new int[10][10];//棋盘信息数组
 	public  int[][] queenMoveAI = new int[10][10];//存放AI棋子能够到达的地方
     public  int[][] queenMoveHI = new int[10][10];//存放HI棋子能够到达的地方
     public  int[][] mobility    = new int[10][10];//存放每个空格的能够达到的位置
-	
+    public  int[][] kingMoveAI  = new int[10][10];//存放AI棋子king走法能到的位置
+    public  int[][] kingMoveHI  = new int[10][10];//存放HI棋子king走法能到的位置
+    
+    /*private void getQueenPosition(){
+    	int one=0;
+    	for(int i=0;i<10;i++){
+    		for(int j=0;j<10;j++){
+    			if(boardArray[i][j]==BLACHCHESS){
+    				chess[one] = 1*100 + 10 *i +j;
+    				one++;
+    			}
+    			else if(boardArray[i][j]==WHITECHESS){
+    				chess[one] =2*100 + 10*i +j;
+    				one++;
+    			}
+    			if(one == 8){
+    				break;
+    			}
+    		}
+    	}
+    }
+    
+    public void queenSearchofMine(){
+    	int i,j,k,l,n;
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				queenMoveAI[i][j]=0;
+				queenMoveHI[i][j]=0;
+			}
+		}
+    }
+	*/
  	public void queenSearch(){
 		int i,j,k,l,n;
 		for(i=0;i<10;i++){
@@ -132,6 +165,141 @@ public class AI {
 			}
 		}
 	}
+
+	/*搜索king走法*/
+	public void kingSearch(){
+		int i,j,n;
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				kingMoveAI[i][j]= 0;
+				kingMoveHI[i][j]= 0;
+			}
+		}
+		for(i=0;i<10;i++)
+		{
+			for(j=0;j<10;j++)
+			{
+				if(boardArray[i][j]==whoIsAI)//white king 1正方棋子
+				{
+					if(boardArray[i-1][j-1]==0)//左上角为空设为可移动
+					{kingMoveAI[i-1][j-1]=1;}
+					
+	                if(boardArray[i-1][j]==0)//正上方为空设为可移动
+					{kingMoveAI[i-1][j]=1;}
+
+	                if(boardArray[i-1][j+1]==0)//右上方为空设为可移动
+					{kingMoveAI[i-1][j+1]=1;}
+
+	                if(boardArray[i][j-1]==0)//左边为空设为可移动
+					{kingMoveAI[i][j-1]=1;}
+
+	                if(boardArray[i][j+1]==0)//右边为空设为可移动
+					{kingMoveAI[i][j+1]=1;}
+
+	                if(boardArray[i+1][j-1]==0)//左下方为空设为可移动
+					{kingMoveAI[i+1][j-1]=1;}
+
+	                if(boardArray[i+1][j]==0)//正下方为空设为可移动
+					{kingMoveAI[i+1][j]=1;}
+
+	                if(boardArray[i+1][j+1]==0)//右下方为空设为可移动
+					{kingMoveAI[i+1][j+1]=1;}
+				}
+
+	            if(boardArray[i][j]==(3-whoIsAI))//反方棋子
+				{
+					if(boardArray[i-1][j-1]==0)
+					{kingMoveHI[i-1][j-1]=1;}
+					
+	                if(boardArray[i-1][j]==0)
+					{kingMoveHI[i-1][j]=1;}
+
+	                if(boardArray[i-1][j+1]==0)
+					{kingMoveHI[i-1][j+1]=1;}
+
+	                if(boardArray[i][j-1]==0)
+					{kingMoveHI[i][j-1]=1;}
+
+	                if(boardArray[i][j+1]==0)
+					{kingMoveHI[i][j+1]=1;}
+
+	                if(boardArray[i+1][j-1]==0)
+					{kingMoveHI[i+1][j-1]=1;}
+
+	                if(boardArray[i+1][j]==0)
+					{kingMoveHI[i+1][j]=1;}
+
+	                if(boardArray[i+1][j+1]==0)
+					{kingMoveHI[i+1][j+1]=1;}
+				}
+			}
+		}
+        
+		//计算10步king走法可以到达的位置
+		for( n=1;n<11;n++)
+		{
+			for(i=0;i<10;i++)
+			{
+				for(j=0;j<10;j++)
+				{
+					if(kingMoveAI[i][j]==n)//white king 2-10
+					{
+						if(boardArray[i-1][j-1]==0&&kingMoveAI[i-1][j-1]==0)
+						{kingMoveAI[i-1][j-1]=n+1;}
+
+	                    if(boardArray[i-1][j]==0&&kingMoveAI[i-1][j]==0)
+						{kingMoveAI[i-1][j]=n+1;}
+
+	                    if(boardArray[i-1][j+1]==0&&kingMoveAI[i-1][j+1]==0)
+						{kingMoveAI[i-1][j+1]=n+1;}
+
+	                    if(boardArray[i][j-1]==0&&kingMoveAI[i][j-1]==0)
+						{kingMoveAI[i][j-1]=n+1;}
+
+	                    if(boardArray[i][j+1]==0&&kingMoveAI[i][j+1]==0)
+						{kingMoveAI[i][j+1]=n+1;}
+
+	                    if(boardArray[i+1][j-1]==0&&kingMoveAI[i+1][j-1]==0)
+						{kingMoveAI[i+1][j-1]=n+1;}
+
+	                    if(boardArray[i+1][j]==0&&kingMoveAI[i+1][j]==0)
+						{kingMoveAI[i+1][j]=n+1;}
+
+	                    if(boardArray[i+1][j+1]==0&&kingMoveAI[i+1][j+1]==0)
+						{kingMoveAI[i+1][j+1]=n+1;}
+					}
+
+	                if(kingMoveHI[i][j]==n)//black king 2-10
+					{
+						if(boardArray[i-1][j-1]==0&&kingMoveHI[i-1][j-1]==0)
+						{kingMoveHI[i-1][j-1]=n+1;}
+
+	                    if(boardArray[i-1][j]==0&&kingMoveHI[i-1][j]==0)
+						{kingMoveHI[i-1][j]=n+1;}
+
+	                    if(boardArray[i-1][j+1]==0&&kingMoveHI[i-1][j+1]==0)
+						{kingMoveHI[i-1][j+1]=n+1;}
+
+	                    if(boardArray[i][j-1]==0&&kingMoveHI[i][j-1]==0)
+						{kingMoveHI[i][j-1]=n+1;}
+
+	                    if(boardArray[i][j+1]==0&&kingMoveHI[i][j+1]==0)
+						{kingMoveHI[i][j+1]=n+1;}
+
+	                    if(boardArray[i+1][j-1]==0&&kingMoveHI[i+1][j-1]==0)
+						{kingMoveHI[i+1][j-1]=n+1;}
+
+	                    if(boardArray[i+1][j]==0&&kingMoveHI[i+1][j]==0)
+						{kingMoveHI[i+1][j]=n+1;}
+
+	                    if(boardArray[i+1][j+1]==0&&kingMoveHI[i+1][j+1]==0)
+						{kingMoveHI[i+1][j+1]=n+1;}
+					}
+				}
+			}
+		}
+	}
+	
 	public void setWhoIsAI(int whoIsAI) {
 		this.whoIsAI = whoIsAI;
 	}
