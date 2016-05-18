e7744bb725cd676ccd9aa7f5688dac977f29b312
Doctoryoung <1875200406@qq.com>
2016-05-18 15:41:54 +0800

    完成
    完成evaluationfunction（）函数的拷贝，不过最后有点问题

--- Amazon/src/amazon/AI.java
+++ Amazon/src/amazon/AI.java
@@ -1,5 +1,5 @@
 package amazon;
-
+//white 对应这里的  AI
 public class AI {
 
 	//public static final int BLACKISAI   = 1;    //黑方是AI
@@ -299,6 +299,246 @@ public class AI {
 			}
 		}
 	}
+	
+	
+	public double t1 =0 ;
+	public void t1account(){
+		int i,j;
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				if(boardArray[i][j]==0){
+					if((queenMoveAI[i][j]==queenMoveHI[i][j])&&(queenMoveAI[i][j]!=0)){//黑白双方皇后走法所需步数相同的点
+						t1=t1+0.1;
+					}
+				    if(queenMoveAI[i][j]<queenMoveHI[i][j]){//AI步数小于HI
+						if(queenMoveAI[i][j]==0){
+							t1=t1-1;
+						}
+					    if(queenMoveAI[i][j]!=0){
+					    	t1=t1+1;
+					    }
+					}
+					if(queenMoveAI[i][j]>queenMoveHI[i][j]){//AI步数大于HI
+						if(queenMoveHI[i][j]==0){
+							t1=t1+1;
+						}
+						if(queenMoveHI[i][j]!=0){
+							t1=t1-1;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public double t2 = 0;
+	public void t2account(){
+		int i,j;
+	    for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				if(boardArray[i][j]==0){
+					if((kingMoveAI[i][j]==kingMoveHI[i][j])&&(kingMoveAI[i][j]!=0)){
+						t2=t2+0.1;
+					}
+				    if(kingMoveAI[i][j]<kingMoveHI[i][j]){
+						if(kingMoveAI[i][j]==0){
+							t2=t2-1;
+						}
+					    if(kingMoveAI[i][j]!=0){
+					    	t2=t2+1;
+					    }
+					}
+					if(kingMoveAI[i][j]>kingMoveHI[i][j]){
+						if(kingMoveHI[i][j]==0){
+							t2=t2+1;
+						}
+						if(kingMoveHI[i][j]!=0){
+							t2=t2-1;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public double c1 = 0;
+	public void c1account(){
+		int i,j;
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				if(boardArray[i][j]==0)
+				{
+					if((queenMoveAI[i][j]!=0)&&(queenMoveHI[i][j]!=0)){
+						c1=c1+Math.pow(2.0,(double)(-(queenMoveAI[i][j])))-Math.pow(2.0,(double)(-(queenMoveHI[i][j])));
+					}
+					if((queenMoveAI[i][j]==0)&&(queenMoveHI[i][j]!=0)){
+						c1=c1-Math.pow(2.0,(double)(-(queenMoveHI[i][j])));
+					}
+					if((queenMoveAI[i][j]!=0)&&(queenMoveHI[i][j]==0)){
+						c1=c1+Math.pow(2.0,(double)(-(queenMoveAI[i][j])));
+					}
+				}
+			}
+		}
+		c1=c1*2;
+	}
+
+	public double c2 = 0;
+	public void c2account(){
+		int i,j;
+		double a;
+	    for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				if(boardArray[i][j]==0){
+					if(kingMoveAI[i][j]!=0&&kingMoveHI[i][j]!=0){
+						a=(kingMoveHI[i][j]-kingMoveAI[i][j]);
+						c2=c2+Math.min(1,(double)Math.max(-1,a/6));
+					}
+					if(kingMoveAI[i][j]==0&&kingMoveHI[i][j]!=0){
+						c2=c2-1;
+					}
+					if(kingMoveAI[i][j]!=0&&kingMoveHI[i][j]==0){
+						c2=c2+1;
+					}
+				}
+			}
+		}
+	}
+
+	public double w = 0;
+	public void wacount(){
+		int i,j;
+		for(i=0;i<10;i++){
+			for(j=0;j<10;j++){
+				if(boardArray[i][j]==0)
+				{
+					if((queenMoveAI[i][j]!=0)&&(queenMoveHI[i][j]!=0))
+					{
+						w=w+Math.pow(2.0,(double)(-(Math.abs((double)(queenMoveAI[i][j]-queenMoveHI[i][j])))));
+					}
+				}
+			}
+		}
+	}
+
+	public int[] awhite = new int[4];
+	public int[] ablack = new int[4];
+	public void aacount(){
+		int i,j,k,l,m;
+		int n=0;
+		int h=0;
+		awhite[0]=awhite[1]=awhite[2]=awhite[3]=ablack[0]=ablack[1]=ablack[2]=ablack[3]=0;
+		
+		//这里的正方，和我们的HI对应可能有问题
+		for(i=0;i<12;i++){
+			for(j=0;j<12;j++){
+				if(boardArray[i][j]==(3-whoIsAI)){//正方，此处代表HI
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l-1]==0)&&(queenMoveHI[k-1][l-1]!=0))
+					{awhite[n]=awhite[n]+mobility[k-1][l-1]/m;k--;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l]==0)&&(queenMoveHI[k-1][l]!=0))
+					{awhite[n]=awhite[n]+mobility[k-1][l]/m;k--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l+1]==0)&&(queenMoveHI[k-1][l+1]!=0))
+					{awhite[n]=awhite[n]+mobility[k-1][l+1]/m;k--;l++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k][l-1]==0)&&(queenMoveHI[k][l-1]!=0))
+					{awhite[n]=awhite[n]+mobility[k][l-1]/m;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k][l+1]==0)&&(queenMoveHI[k][l+1]!=0))
+					{awhite[n]=awhite[n]+mobility[k][l+1]/m;l++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l-1]==0)&&(queenMoveHI[k+1][l-1]!=0))
+					{awhite[n]=awhite[n]+mobility[k+1][l-1]/m;k++;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l]==0)&&(queenMoveHI[k+1][l]!=0))
+					{awhite[n]=awhite[n]+mobility[k+1][l]/m;k++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l+1]==0)&&(queenMoveHI[k+1][l+1]!=0))
+					{awhite[n]=awhite[n]+mobility[k+1][l+1]/m;k++;l++;m=m*2;}
+					n++;
+				}
+
+				if(boardArray[i][j]==whoIsAI){
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l-1]==0)&&(queenMoveAI[k-1][l-1]!=0))
+					{ablack[h]=ablack[h]+mobility[k-1][l-1]/m;k--;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l]==0)&&(queenMoveAI[k-1][l]!=0))
+					{ablack[h]=ablack[h]+mobility[k-1][l]/m;k--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k-1][l+1]==0)&&(queenMoveAI[k-1][l+1]!=0))
+					{ablack[h]=ablack[h]+mobility[k-1][l+1]/m;k--;l++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k][l-1]==0)&&(queenMoveAI[k][l-1]!=0))
+					{ablack[h]=ablack[h]+mobility[k][l-1]/m;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k][l+1]==0)&&(queenMoveAI[k][l+1]!=0))
+					{ablack[h]=ablack[h]+mobility[k][l+1]/m;l++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l-1]==0)&&(queenMoveAI[k+1][l-1]!=0))
+					{ablack[h]=ablack[h]+mobility[k+1][l-1]/m;k++;l--;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l]==0)&&(queenMoveAI[k+1][l]!=0))
+					{ablack[h]=ablack[h]+mobility[k+1][l]/m;k++;m=m*2;}
+
+					k=i;l=j;m=1;
+					while((boardArray[k+1][l+1]==0)&&(queenMoveAI[k+1][l+1]!=0))
+					{ablack[h]=ablack[h]+mobility[k+1][l+1]/m;k++;l++;m=m*2;}
+					h++;
+				}
+			}
+		}
+	}
+
+	public int ec = 0;
+	public double evaluation(){
+		ec++;
+		double value;
+		nside=side;
+		kingSearch();
+		queenSearch();
+		mobilitysearch();
+		t1account();t2account();c1account();c2account();
+		wacount();
+		//aaccount();
+		// t1=rand()%5; t2=rand()%15;  c1=rand()%15; c2=rand()%125;
+		double a=(5/(w+5));
+		double b=(w/(w+20));
+		double c=(1-(a+b))/2;
+//	    double m=(w/250)*(awhite[0]+awhite[1]+awhite[2]+awhite[3])-(w/200)*(ablack[0]+ablack[1]+ablack[2]+ablack[3]);
+//	    cout<<"a,b,c,w,m"<<endl;
+//		cout<<a<<" "<<b<<" "<<c<<" "<<w<<" "<<m<<endl;
+//		cout<<"t1,t2,c1,c2"<<endl;
+//		cout<<t1<<" "<<t2<<" "<<c1<<" "<<c2<<endl;
+
+		value=a*t1+b*(t2/2)+c*((c1+c2)/2);	
+		if(side==0)
+			return value;
+		else
+			return -value;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	public void setWhoIsAI(int whoIsAI) {
 		this.whoIsAI = whoIsAI;
